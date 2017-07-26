9292ace595128a7549078a7908e89b0da6dfad68
huynd0612 <nguyendanghuy0612@gmail.com>
2017-07-26 13:17:45 +0700

    Lesson 13: apply layout

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { mainRoutes } from './main.routes';
 import { UserModule } from './user/user.module';
+import { HomeModule } from './home/home.module';
+
 @NgModule({
   imports: [
     CommonModule,
     UserModule,
+    HomeModule,
     RouterModule.forChild(mainRoutes)
   ],
   declarations: [MainComponent]
--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -6,6 +6,8 @@ export const mainRoutes: Routes = [
         path: '', component: MainComponent, children: [
             // localhost:4200/main
             { path: '', redirectTo: 'home', pathMatch: 'full' },
+             // localhost:4200/main/home
+            { path: 'home', loadChildren: './home/home.module#HomeModule' },
             // localhost:4200/main/user
             { path: 'user', loadChildren: './user/user.module#UserModule' }
         ]
